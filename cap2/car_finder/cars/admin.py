8b203c29d1c29a377f0343306f6a23d3f25dd9d7
nabeellaham <kameln111@gmail.com>
2025-10-15 10:33:33 +0300

    .

--- cap2/car_finder/cars/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Car
-
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ['brand', 'model', 'year', 'price', 'user', 'is_sold', 'created_at',]
-    list_filter = ['brand', 'car_type', 'is_sold', 'created_at']
-    search_fields = ['brand', 'model', 'user__username']
-    
